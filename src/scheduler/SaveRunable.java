4c43accd4d4a9ae83b4d9efaf23c16164506b9bf
Sylvain Goulliart <Sylvain.goulliart@gmail.com>
2018-04-13 13:59:25 +0100

    clean

--- src/scheduler/SaveRunable.java
+++ src/scheduler/SaveRunable.java
@@ -1,18 +1,12 @@
 package scheduler;
 
 import bot.domain.Data;
-import bot.domain.Datas;
-
-import static java.lang.String.format;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 
 public class SaveRunable implements Runnable {
-    public final static String PRESENCE_FOLDER = "save/presence/";
-    public final static String PRESENCE_FILE = "%s.sav";
-    public final static String PLAYER_FILE = "save/player.sav";
     public final static String DATA_FILE = "save/data.sav";
 
     Data data;
@@ -24,15 +18,7 @@ public class SaveRunable implements Runnable {
     @Override
     public void run() {
         try {
-            File file = new File(PRESENCE_FOLDER);
-            if(file.exists()){
-                file.delete();
-            }
-            file = new File(PLAYER_FILE);
-            if(file.exists()){
-                file.delete();
-            }
-            file = new File(DATA_FILE);
+            File file = file = new File(DATA_FILE);
             FileOutputStream fos = new FileOutputStream(file);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(data);
