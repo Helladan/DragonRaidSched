8612ae967ef3750adca0950aa5c99ffbfb6d29ba
goulliarts <goulliarts@FR-L2986517.groupinfra.com>
2018-09-13 13:12:34 +0200

    Maj des massages d'aide

--- src/bot/domain/Messages.java
+++ src/bot/domain/Messages.java
@@ -7,11 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Messages {
-	START(ProcessMessage.START + " j hh:mm d\n"
+	START(ProcessMessage.START + " j hh:mm d ou " + ProcessMessage.START + " -s j hh:mm\n"
+			+ " - [-s] pour une seul soirée (mode singleton)"
 			+ " - j : jour en toutes lettres (lundi, mardi, mercredi, etc...)\n"
-			+ " - hh:mm : heure de dï¿½but (format 24h)\n"
+			+ " - hh:mm : heure de début (format 24h)\n"
 			+ " - d : durÃ©e du raid en heure (c'est le temps avant la prochaine annonce)"),
-	DROIT("Tu n'as pas les droits pour cette commande (GÃ©rÃ© les messages)"),
+	DROIT("Tu n'as pas les droits pour cette commande (Géré les messages)"),
 	NOT_START("Aucune annonce de faite."),
 	MAUVAISE_COMMANDE("Commande non-comprise"),
 	TAPE_HELP("tape " + ProcessMessage.HELP + " pour de l'aide"),
