e1458f87e378473fad12a4cd03be6bfd2f2e30bf
Sylvain Goulliart <Sylvain.goulliart@gmail.com>
2019-01-04 19:36:45 +0100

    Update Messages.java

--- src/bot/domain/Messages.java
+++ src/bot/domain/Messages.java
@@ -8,16 +8,17 @@ import lombok.Getter;
 @Getter
 public enum Messages {
 	START(ProcessMessage.START + " j hh:mm d ou " + ProcessMessage.START + " -s j hh:mm\n"
-			+ " - [-s] pour une seul soir�e (mode singleton)"
+			+ " - [-s] pour une seul soirée (mode singleton)"
 			+ " - j : jour en toutes lettres (lundi, mardi, mercredi, etc...)\n"
-			+ " - hh:mm : heure de d�but (format 24h)\n"
-			+ " - d : durée du raid en heure (c'est le temps avant la prochaine annonce)"),
-	DROIT("Tu n'as pas les droits pour cette commande (G�r� les messages)"),
+			+ " - hh:mm : heure de début (format 24h)\n"
+			+ " - d : durÃ©e du raid en heure (c'est le temps avant la prochaine annonce)"),
+	DROIT("Tu n'as pas les droits pour cette commande (Géré les messages)"),
 	NOT_START("Aucune annonce de faite."),
 	MAUVAISE_COMMANDE("Commande non-comprise"),
 	TAPE_HELP("tape " + ProcessMessage.HELP + " pour de l'aide"),
 	INSCRITPION("merci de t'inscrire (entre les emotes des divers classes que tu peux jouer en Raid)"),
-	OVERFLOW("10 personnes sont déjà inscrites, tu seras en reserve"),
-	NO_INSCRIPTION("Aucune inscription n'avait été faite");
+	OVERFLOW("10 personnes sont dÃ©jÃ  inscrites, tu seras en reserve"),
+	BALANCED("\"Perfectly balanced, as all things should be.\""),
+	NO_INSCRIPTION("Aucune inscription n'avait Ã©tÃ© faite");
 	private String message; 
 }
