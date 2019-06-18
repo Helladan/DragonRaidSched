57257db0fb33eb5052207ad6c5368d136c2cdf5d
Anthony Godard <anthonygodard@gmail.com>
2019-06-18 13:55:14 +0200

    - Typo - Intellij files add to .gitignore

--- src/bot/domain/Messages.java
+++ src/bot/domain/Messages.java
@@ -8,16 +8,16 @@ import lombok.Getter;
 @Getter
 public enum Messages {
 	START(ProcessMessage.START + " j hh:mm d ou " + ProcessMessage.START + " -s j hh:mm\n"
-			+ " - [-s] pour une seul soirée (mode singleton)"
+			+ " - [-s] pour une seule soirée (mode singleton)"
 			+ " - j : jour en toutes lettres (lundi, mardi, mercredi, etc...)\n"
 			+ " - hh:mm : heure de début (format 24h)\n"
-			+ " - d : durÃ©e du raid en heure (c'est le temps avant la prochaine annonce)"),
-	DROIT("Tu n'as pas les droits pour cette commande (Géré les messages)"),
+			+ " - d : durée du raid en heure (c'est le temps avant la prochaine annonce)"),
+	DROIT("Tu n'as pas les droits pour cette commande (Gérer les messages)"),
 	NOT_START("Aucune annonce de faite."),
 	MAUVAISE_COMMANDE("Commande non-comprise"),
 	TAPE_HELP("tape " + ProcessMessage.HELP + " pour de l'aide"),
-	INSCRITPION("merci de t'inscrire (entre les emotes des divers classes que tu peux jouer en Raid)"),
-	OVERFLOW("10 personnes sont dÃ©jÃ  inscrites, tu seras en reserve"),
-	NO_INSCRIPTION("Aucune inscription n'avait Ã©tÃ© faite");
+	INSCRITPION("Merci de t'inscrire (indique les emotes des diverses classes que tu peux jouer en Raid)"),
+	OVERFLOW("10 personnes sont déjà  inscrites, tu seras en reserve"),
+	NO_INSCRIPTION("Aucune inscription n'avait été faite");
 	private String message; 
 }
